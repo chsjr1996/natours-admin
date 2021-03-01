52a55ced5e579c6a8564886029b263676c5b8fcf
Carlos H <chsjr1996@gmail.com>
2021-03-01 01:41:56 -0300

    Fix tours-list tests
    - Import MatTableModule inside testing module in spec.ts file

--- src/app/tours/tours-list/tours-list.component.spec.ts
+++ src/app/tours/tours-list/tours-list.component.spec.ts
@@ -3,13 +3,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToursListComponent } from './tours-list.component';
 import { ToursService } from '../shared/tours.service';
 import { TestToursService } from 'src/testing/test-tours.service';
+import { MatTableModule } from '@angular/material/table';
 
 describe('ToursListComponent', () => {
   let component: ToursListComponent;
   let fixture: ComponentFixture<ToursListComponent>;
 
+  const mat = [MatTableModule];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [...mat],
       providers: [
         {
           provide: ToursService,
