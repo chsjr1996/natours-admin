ce810855798a82412ad99ee9615698c0e3534181
Carlos H <chsjr1996@gmail.com>
2021-03-01 01:26:46 -0300

    Fix tours module tests
    - Tours Service tests fixed
    - Tours List Component tests fixed

--- src/app/tours/shared/tours.service.spec.ts
+++ src/app/tours/shared/tours.service.spec.ts
@@ -1,16 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-
-import { ToursService } from './tours.service';
+import { Tour, ToursService } from './tours.service';
+import { asyncData } from 'src/testing/async-observable-helpers';
+import { getTestTours } from 'src/testing/test-tours';
 
 describe('ToursService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
   let service: ToursService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ToursService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new ToursService(httpClientSpy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return expected tours (HttpClient called once', () => {
+    const expectedTours: Tour[] = getTestTours();
+
+    httpClientSpy.get.and.returnValue(asyncData(expectedTours));
+
+    service
+      .getTours()
+      .subscribe(
+        (tours) => expect(tours).toEqual(expectedTours, 'expected tours'),
+        fail
+      );
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 });
